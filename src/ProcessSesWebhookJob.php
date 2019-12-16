493561548b64143a5d0227fb74b3eec87a6ac4d3
freek <freek@spatie.be>
2019-12-16 09:08:47 +0100

    wip

--- src/ProcessSesWebhookJob.php
+++ src/ProcessSesWebhookJob.php
@@ -4,10 +4,18 @@ namespace Spatie\MailcoachSesFeedback;
 
 use Illuminate\Support\Arr;
 use Spatie\Mailcoach\Models\Send;
+use Spatie\WebhookClient\Models\WebhookCall;
 use Spatie\WebhookClient\ProcessWebhookJob;
 
 class ProcessSesWebhookJob extends ProcessWebhookJob
 {
+    public function __construct(WebhookCall $webhookCall)
+    {
+        parent::__construct($webhookCall);
+
+        $this->queue = config('mailcoach.perform_on_queue.process_feedback_job');
+    }
+
     public function handle()
     {
         $payload = json_decode($this->webhookCall->payload['Message'], true);
