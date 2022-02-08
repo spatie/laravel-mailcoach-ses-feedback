83fc75712b8a39d7073f940756bc83333cbd43ec
riasvdv <rias@spatie.be>
2022-02-08 16:37:04 +0000

    wip

--- tests/AddConfigurationSetHeaderTest.php
+++ tests/AddConfigurationSetHeaderTest.php
@@ -3,14 +3,15 @@
 namespace Spatie\MailcoachSesFeedback\Tests;
 
 use Illuminate\Mail\Events\MessageSending;
-use Swift_Message;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\TextPart;
 
 class AddConfigurationSetHeaderTest extends TestCase
 {
     /** @test **/
     public function it_adds_a_configuration_set_header_if_the_message_is_sent_by_mailcoach()
     {
-        $message = new Swift_Message('Test', 'body');
+        $message = (new Email())->setBody(new TextPart('body'));
         $message->getHeaders()->addTextHeader('X-MAILCOACH', true);
 
         config()->set('mailcoach.ses_feedback.configuration_set', 'hello');
@@ -19,6 +20,6 @@ class AddConfigurationSetHeaderTest extends TestCase
 
         event(new MessageSending($message));
 
-        $this->assertEquals('hello', $message->getHeaders()->get('X-SES-CONFIGURATION-SET')->getFieldBody());
+        $this->assertEquals('hello', $message->getHeaders()->get('X-SES-CONFIGURATION-SET')->getBodyAsString());
     }
 }
--- tests/StoreTransportMessageIdTest.php
+++ tests/StoreTransportMessageIdTest.php
@@ -5,7 +5,8 @@ namespace Spatie\MailcoachSesFeedback\Tests;
 use Illuminate\Mail\Events\MessageSent;
 use Spatie\Mailcoach\Domain\Shared\Models\Send;
 use Spatie\MailcoachSesFeedback\Tests\factories\SendFactory;
-use Swift_Message;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\TextPart;
 
 class StoreTransportMessageIdTest extends TestCase
 {
@@ -13,7 +14,7 @@ class StoreTransportMessageIdTest extends TestCase
     public function it_stores_the_message_id_from_the_transport()
     {
         $pendingSend = (new SendFactory())->create();
-        $message = new Swift_Message('Test', 'body');
+        $message = (new Email())->setBody(new TextPart('body'));
         $message->getHeaders()->addTextHeader('X-Ses-Message-ID', '1234');
 
         event(new MessageSent($message, [
